2519a1df1f05aade0badbf298b19e4802eb6ad19
Ankit Patnaik <ankit.123patnaik123@gmail.com>
2023-03-03 10:32:01 +0530

    adding environment variable

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ const encrypt = require("mongoose-encryption");
 
 const app = express();
 
+console.log(process.env.SECRET);
+
 
 app.use(express.static("public"));
 app.set('view engine','ejs');
@@ -21,8 +23,8 @@ const userSchema = new mongoose.Schema({
     password: String
 });
 
-const secret = "Accesstothesecret";
-userSchema.plugin(encrypt, { secret: secret, encryptedFields: ["password"] });
+
+userSchema.plugin(encrypt, { secret: process.env.SECRET, encryptedFields: ["password"] });
 
 
 const User = new mongoose.model("User",userSchema);
